9c98e997abc38ae8f3fa84a3b7d164420fbedbd0
pawelz <t03c10@gmail.com>
2018-02-21 21:26:11 +0100

    task done

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,18 @@
-
-
 var a = prompt('Enter value for "a"'), b = prompt('Enter value for "b"'), value, valueType;
-value=(a * a) - (2 * a * b) - (b * b);
+value = (a * a) - (2 * a * b) - (b * b);
 
-if (value > 0) { 
+if (value > 0) {
 	valueType = 'Wynik dodatni';
-} 
-	else if (value == 0 ) { 
-		valueType = 'Wynik zerowy';
-	} 
-	else if (value < 0) { 
-		valueType = 'Wynik ujemny';
-	} 
-	else { 
-		valueType = 'Wynik bledny';
-	}
+}
+else if (value == 0) {
+	valueType = 'Wynik zerowy';
+}
+else if (value < 0) {
+	valueType = 'Wynik ujemny';
+}
+else {
+	valueType = 'Wynik bledny';
+}
 
 console.log('Calculation result is: ' + value);
 console.log(valueType);
